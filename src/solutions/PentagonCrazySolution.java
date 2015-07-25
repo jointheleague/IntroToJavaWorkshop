2cf71bc6369902e7bf71a3a6d847429c830f8530
league-workshopper <workshop@AdministorsMini.attlocal.net>
2015-07-25 14:57:34 -0700

    Adjusting recipe solutions

--- src/solutions/PentagonCrazySolution.java
+++ src/solutions/PentagonCrazySolution.java
@@ -1,5 +1,7 @@
 package solutions;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /* Teacher’s note: before beginning, draw a pentagon and have students work out the angle that the robot will have to turn (360/5) */
@@ -14,7 +16,7 @@ public class PentagonCrazySolution {
 		// 8. make the robot go at maximum speed
 		robot.setSpeed(10);
 		// 9. choose a color that you like for the shape
-		robot.setPenColor(30, 130, 30);
+		robot.setPenColor(Color.GREEN);
 		// 4. make a variable for the number of sides you want (can’t test this one)
 		int sides = 5;
 		// 5. make a variable for the angle you want the robot to turn. Hint: you can divide in Java using “/”. Can’t test until step 6.
