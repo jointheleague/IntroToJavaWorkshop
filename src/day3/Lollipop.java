0dab0f5e1b71450370bc10a082e1c637cc11a25d
Wintress Mac <DON'T CHANGE THIS>
2016-07-27 14:53:31 -0700

    hi

---
+++ src/day3/Lollipop.java
@@ -0,0 +1,12 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class Lollipop {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello!");
+		System.out.println("hello again");
+	}
+
+}
